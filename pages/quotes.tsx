f589aa79d6e223299b50b788fc2f5fbfc20ea0cf
Willie Chalmers III <willie.chalmers@utdallas.edu>
2022-07-04 18:02:21 -0500

    feat: Add OGP tags to /quotes page

--- pages/quotes.tsx
+++ pages/quotes.tsx
@@ -1,7 +1,7 @@
-import type { NextPage } from "next";
-import Head from "next/head";
-import supabase from "../lib/hooks/supabase";
-import SiteFooter from "../components/SiteFooter";
+import type { NextPage } from 'next';
+import Head from 'next/head';
+import supabase from '../lib/hooks/supabase';
+import SiteFooter from '../components/SiteFooter';
 import { useQuotes } from '../lib/quotes';
 import QuoteList from '../components/QuoteList';
 
@@ -16,12 +16,16 @@ const HomePage: NextPage = () => {
           name="description"
           content="See all the amusing things Clarkies have said during the 2022 Clark Summer Research Program."
         />
+        <link rel="icon" href="/favicon.ico" />
+
         <meta
           name="og:site_name"
           content="Clark Summer Research Program Dashboard"
         />
         <meta name="og:title" content="Quote Tracker" />
-        <link rel="icon" href="/favicon.ico" />
+        <meta name="og:image" content="/meta-thumbnails/clark-quotes.jpg" />
+        <meta property="og:image:type" content="image/jpeg" />
+        <meta property="og:image:alt" content="" />
       </Head>
       <header className="p-12 text-4xl font-bold text-center bg-blue-200 dark:bg-blue-800">
         Clark Quote Tracker
